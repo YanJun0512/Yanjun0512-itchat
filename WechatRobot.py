9430d0ff44d89f985ac9bbe7e5f93309f37fc53a
YanJun0512 <Yanjun0512@outlook.com>
2018-01-19 18:55:20 +0800

    Update WechatRobot.py

--- WechatRobot.py
+++ WechatRobot.py
@@ -16,8 +16,8 @@ def get_response(msg, FromUserName):
         password = msg_match.group(2)
         return CallGrades.query_grades(username, password, 2017, 3)
 
-    api_url = 'http://www.tuling123.com/openapi/api'
-    apikey = 'cb7ef07f100a4a73a192685bd52f116b'
+    api_url = '图灵机器人api地址'
+    apikey = '你的api_key'
     hash = hashlib.md5()
     userid = hash.update(FromUserName.encode('utf-8'))
     # print('userid=', userid)
@@ -75,28 +75,6 @@ def add_friend(msg):
     itchat.add_friend(msg)
     itchat.send_msg('Nice To Meet You !', msg['RecommendInfo']['UserName'])
 
-
-
-# 群聊
-Message = '震惊！西大官微居然做出这种事'
-GroupsContainer = set()
-
-'''
-@itchat.msg_register('Text', isGroupChat=True)
-def broadcast(msg):
-    response = get_response(msg['Content'], msg['FromUserName'])
-    itchat.send(response, msg['FromUserName'])
-    print('发送群：', msg['FromUserName'])
-    groups_json_list = itchat.get_chatrooms()
-    groupsName = [nm.get('UserName') for nm in groups_json_list]
-    groupsName = set(groupsName)
-    for grpn in groupsName:
-        GroupsContainer.add(grpn)
-    current_time = time.localtime(time.time())
-    if (current_time.tm_hour == 15 and swatch == 1):
-        for grn in GroupsContainer:
-            itchat.send(Message, grn)
-        swatch = 0'''
 
 if __name__ == '__main__':
     itchat.run()
